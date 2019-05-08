a6ea03e5dba9593ed80ff896780643093f191665
Navdeep Sekhon <sekhon@navdeep.co>
2019-05-08 18:35:00 -0400

    add a simple test to check the cluster

--- src/test/java/ConfiguratorTest.java
+++ src/test/java/ConfiguratorTest.java
@@ -1,12 +1,26 @@
 import co.navdeep.kafkaer.Configurator;
 import co.navdeep.kafkaer.model.Config;
+import co.navdeep.kafkaer.utils.Utils;
+import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.kafka.clients.admin.AdminClient;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class ConfiguratorTest {
+
+    static AdminClient adminClient;
+
+    @BeforeClass
+    public static void setup() throws ConfigurationException {
+        Configuration properties = Utils.readProperties("src/test/resources/test.properties");
+        adminClient = AdminClient.create(Utils.getClientConfig(properties));
+    }
+
     @Test
     public void testReadConfig() throws IOException, ConfigurationException {
         Configurator configurator = new Configurator("src/test/resources/test.properties", "src/test/resources/kafka-config.json");
@@ -16,5 +30,11 @@ public class ConfiguratorTest {
         Assert.assertEquals(config.getTopics().get(0).getPartitions(), 3);
         Assert.assertEquals(config.getTopics().get(0).getReplicationFactor(), 3);
         Assert.assertEquals(config.getTopics().get(0).getConfigs().get("compression.type"), "gzip");
+    }
+
+    @Test
+    public void testTopicCreation() throws ExecutionException, InterruptedException, IOException, ConfigurationException {
+        Configurator configurator = new Configurator("src/test/resources/test.properties", "src/test/resources/kafka-config.json");
+        configurator.applyConfig();
     }
 }
